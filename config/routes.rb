dd42e74639d13c8d301fa7a528549b64baacd3da
Mosa6 <tikamongi@gmail.com>
2022-06-12 04:23:01 +0000

    [update]install-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  scope module: :user do
+    root to: 'homes#top'
+    get 'homes/about'
+  end
+  devise_for :users, controllers: {
+    registrations: 'user/registrations',
+    sessions: "user/sessions",
+  }
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
