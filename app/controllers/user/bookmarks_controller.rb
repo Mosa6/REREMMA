8c4ad95fca69b47140cccd9bebcedd9c929551b7
Mosa6 <tikamongi@gmail.com>
2022-06-18 03:23:44 +0000

    [update]ブックマーク非同期化

--- app/controllers/user/bookmarks_controller.rb
+++ app/controllers/user/bookmarks_controller.rb
@@ -2,23 +2,16 @@ class User::BookmarksController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @post = Post.find(params[:post_id])
-    bookmark = @post.bookmarks.new(user_id: current_user.id)
-    if bookmark.save
-      redirect_to request.referer
-    else
-      redirect_to request.referer
-    end
+    post = Post.find(params[:post_id])
+    @bookmark = current_user.bookmarks.new(post_id: post.id)
+    @bookmark.save
+    render 'replace_btn'
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
-    bookmark = @post.bookmarks.find_by(user_id: current_user.id)
-    if bookmark.present?
-        bookmark.destroy
-        redirect_to request.referer
-    else
-        redirect_to request.referer
-    end
+    post = Post.find(params[:post_id])
+    @bookmark = current_user.bookmarks.find_by(post_id: post.id)
+    @bookmark.destroy
+    render 'replace_btn'
   end
 end
